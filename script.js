5dce4c8f57f5db0da4ea5e13d6880931c1f5d15b
Ibne-Shahid <anasbinshahid2004@gmail.com>
2025-09-05 21:32:34 +0600

    dynamic words by level option added

--- script.js
+++ script.js
@@ -4,17 +4,54 @@ const loadLessons = () => {
         .then(json => displayLesson(json.data))
 }
 
+const displayWords = (id)=>{
+    fetch(`https://openapi.programming-hero.com/api/level/${id}`)
+    .then(res => res.json())
+    .then(data => wordLessons(data.data))
+}
+
+const wordLessons = (words)=>{
+
+    const wordContainer = document.getElementById("word-container")
+    wordContainer.innerHTML = ""
+
+    words.forEach((word) =>{
+        
+        const card = document.createElement("div")
+
+        card.innerHTML= `
+            <div
+        class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4"
+      >
+      <h2 class="font-bold text-2xl">${word.word}</h2>
+      <p class="font-semibold">Meaning /Pronounciation</p>
+      <div class="text-2xl font-medium font-bangla">${word.meaning}/${word.pronunciation}</div>
+       <div class="flex justify-between items-center">
+       <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <i class="fa-solid fa-circle-info"></i>
+          </button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <i class="fa-solid fa-volume-high"></i>
+          </button>
+       </div>
+      </div>
+        `
+        wordContainer.appendChild(card)
+    })
+    
+
+}
+
 const displayLesson = (lessons) => {
 
     const levelContainer = document.getElementById("level-container")
     levelContainer.innerHTML = ""
 
-    for(let lesson of lessons){
-        console.log(lesson);
-        
+    for (let lesson of lessons) {
+
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-            <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
+            <button onclick="displayWords(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
         `
         levelContainer.appendChild(btnDiv)
     }
