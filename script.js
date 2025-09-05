8722e43cbe9c7b0da8f68a454afa5a4432e1b904
Ibne-Shahid <anasbinshahid2004@gmail.com>
2025-09-05 23:12:20 +0600

    active button and daisyUI modal added

--- script.js
+++ script.js
@@ -4,16 +4,40 @@ const loadLessons = () => {
         .then(json => displayLesson(json.data))
 }
 
+const removeActiveClass = ()=>{
+
+    const removeActiveClass = document.querySelectorAll(".remove-btn")
+    removeActiveClass.forEach((btn)=>btn.classList.remove("active"))
+
+}
+
 const displayWords = (id)=>{
     fetch(`https://openapi.programming-hero.com/api/level/${id}`)
     .then(res => res.json())
     .then(data => wordLessons(data.data))
+    removeActiveClass()
+    const clickBtn = document.getElementById(`click-btn-${id}`)
+    clickBtn.classList.add("active")
 }
 
 const wordLessons = (words)=>{
 
     const wordContainer = document.getElementById("word-container")
     wordContainer.innerHTML = ""
+
+    if(words.length===0){
+        wordContainer.innerHTML= `
+            <div
+        class="text-center  col-span-full rounded-xl py-10 space-y-6 font-bangla"
+      >
+        <img class="mx-auto" src="./assets/alert-error.png"/>
+        <p class="text-xl font-medium text-gray-400">
+          এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+        </p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+      </div>
+        `
+    }
 
     words.forEach((word) =>{
         
@@ -23,11 +47,11 @@ const wordLessons = (words)=>{
             <div
         class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4"
       >
-      <h2 class="font-bold text-2xl">${word.word}</h2>
+      <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
       <p class="font-semibold">Meaning /Pronounciation</p>
-      <div class="text-2xl font-medium font-bangla">${word.meaning}/${word.pronunciation}</div>
+      <div class="text-2xl font-medium font-bangla">${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"}/${word.pronunciation ? word.pronunciation : "Pronounciation পাওয়া  যায়নি"}</div>
        <div class="flex justify-between items-center">
-       <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+       <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
             <i class="fa-solid fa-circle-info"></i>
           </button>
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
@@ -51,7 +75,7 @@ const displayLesson = (lessons) => {
 
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-            <button onclick="displayWords(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
+            <button id="click-btn-${lesson.level_no}" onclick="displayWords(${lesson.level_no})" class="btn btn-outline btn-primary remove-btn"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
         `
         levelContainer.appendChild(btnDiv)
     }
